17487b0b997286ecfd30ba328bbcd8a4cc02f564
Nikola Davidovac <davidovacnikola@gmail.com>
2025-07-08 13:30:36 +0200

    Konacna prerada modela rezervacije(valjda)

--- app/tours/model/reservation.model.ts
+++ app/tours/model/reservation.model.ts
@@ -1,13 +1,17 @@
+import { Tour } from "./tour.model"
+
 export class Reservation {
     public id: number
     public guestsCount: number
     public userId: number
     public tourId: number
+    public tour: Tour
 
-    constructor(id, guestsCount, userId, tourId) {
+    constructor(id, guestsCount, userId, tourId, tour) {
         this.id = id
         this.guestsCount = guestsCount
         this.userId = userId
         this.tourId = tourId
+        this.tour = tour
     }
 }
