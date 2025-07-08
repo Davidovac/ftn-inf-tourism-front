7d501da6ecffa2ab753835556962c5bfbdec5a31
Nikola Davidovac <davidovacnikola@gmail.com>
2025-07-08 13:25:18 +0200

    Uklonjena neka nepotrebna polja iz modela rezervacije i dodat folder i fajlovi za pregled jedne ture

--- app/tours/model/reservation.model.ts
+++ app/tours/model/reservation.model.ts
@@ -1,20 +1,13 @@
-import { User } from "../../users/model/user.model.js"
-import { Tour } from "./tour.model.js"
-
 export class Reservation {
     public id: number
     public guestsCount: number
-    public user?: User
     public userId: number
-    public tour: Tour
     public tourId: number
 
-    constructor(id, guestsCount, user, userId, tour, tourId) {
+    constructor(id, guestsCount, userId, tourId) {
         this.id = id
         this.guestsCount = guestsCount
-        this.user = user
         this.userId = userId
-        this.tour = tour
         this.tourId = tourId
     }
 }
--- app/tours/model/reservationFormData.model.ts
+++ app/tours/model/reservationFormData.model.ts
@@ -1,11 +1,6 @@
-import { User } from "../../users/model/user.model.js";
-import { Tour } from "./tour.model.js";
-
 export interface ReservationFormData {
   id: number;
   guestsCount: number;
-  user?: User;
   userId: number;
-  tour: Tour;
   tourId: number;
 }
